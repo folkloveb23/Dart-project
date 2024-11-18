eefc814087137a5489f546f49460a4a2d3059a3a
dart-project <folklovec14@gmail.com>
2024-11-18 16:10:50 +0700

    2

--- hello.dart
+++ hello.dart
@@ -1,3 +1,3 @@
 void main(){
-  print('Hello wprld');
+  print('Hello world');
 }
---
+++ remainder.dart
@@ -0,0 +1,11 @@
+void main() {
+ 
+  int A = 10;  
+  int B = 3;    
+ 
+  int quotient = A ~/ B;  
+  int remainder = A % B;
+ 
+  print('ผลหาร (Quotient) = $quotient');
+  print('เศษ (Remainder) = $remainder');
+}
